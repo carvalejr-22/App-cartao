86438bb6a82f36c9c2a36b460adf7f804ac0d857
carvalejr-22 <carvalejr@gmail.com>
2026-09-11 21:44:50 -0300

    Bump Meu Cartao to v0.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.carlos.appcartao"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.8.0"
+        versionCode = 14
+        versionName = "0.8.1"
     }
 
     buildFeatures {
